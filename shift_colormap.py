730c36ce6054affc298433318cd9449ded50a5dc
David W <dcw32@cam.ac.uk>
2015-07-16 14:05:18 +0100

    Ack

--- shift_colormap.py
+++ shift_colormap.py
@@ -1,5 +1,7 @@
 # Shift the colormap to a defined centre, for example when doing
 # a change plot where there's a bias (e.g. changes between -0.1 to 0.3)
+
+# Adapted from somewhere online...
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,25 +14,19 @@ def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
         'blue': [],
         'alpha': []
     }
-
     # regular index to compute the colors
     reg_index = np.linspace(start, stop, 257)
-
     # shifted index to match the data
     shift_index = np.hstack([
         np.linspace(0.0, midpoint, 128, endpoint=False), 
         np.linspace(midpoint, 1.0, 129, endpoint=True)
     ])
-
     for ri, si in zip(reg_index, shift_index):
         r, g, b, a = cmap(ri)
-
         cdict['red'].append((si, r, r))
         cdict['green'].append((si, g, g))
         cdict['blue'].append((si, b, b))
         cdict['alpha'].append((si, a, a))
-
     newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
     plt.register_cmap(cmap=newcmap)
-
     return newcmap
