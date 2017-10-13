31c3b3887b2d116735a28fb3c506ffd63f9ae183
dcw32 <dcw32@cam.ac.uk>
2017-10-13 15:21:37 +0100

    Minimal example

--- modules.py
+++ modules.py
@@ -6,7 +6,21 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import AxesGrid
-
+from mpl_toolkits.basemap import Basemap, shiftgrid, addcyclic
+import math as m
+def latlon_shift(lons,lats,data):
+        lonsi=lons-360
+        data,lonsi=addcyclic(data,lonsi)
+        data,lonsi=shiftgrid(-180.,data,lonsi,cyclic=360.)
+        lonsi,latsi=np.meshgrid(lonsi,lats)
+        return(lonsi,latsi,data)
+def gbox_areas(x,y):
+# lats x lons
+        area=np.zeros([x,y])
+        R=6.371E6
+        for j in range(x):
+                area[j,:]=(R**2)*m.radians(360./y)*(m.sin(m.radians(90.-(j-0.5)*180./(x-1)))-m.sin(m.radians(90.-(180./(x-1))*(j+0.5))))
+        return area
 def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
     cdict = {
         'red': [],
